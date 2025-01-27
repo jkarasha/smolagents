a84f2e930ceec5aa06a507e45b37659c1cde1aae
Joe Karasha <jkarasha@gmail.com>
2025-01-26 20:58:51 -0800

    update to use HF tutorial.

--- hello.py
+++ hello.py
@@ -1,6 +1,13 @@
-def main():
-    print("Hello from smolagents!")
+# !pip install smolagents[litellm]
+from smolagents import CodeAgent, LiteLLMModel
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 
-if __name__ == "__main__":
-    main()
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+model = LiteLLMModel(model_id="openai/gpt-4o", api_key=OPENAI_API_KEY) # Could use 'gpt-4o'
+agent = CodeAgent(tools=[], model=model, add_base_tools=True)
+
+agent.run("Could you give me the 118th number in the Fibonacci sequence?")

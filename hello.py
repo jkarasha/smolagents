30864201f982f7a73451f5212e38b80a12637dca
Joe Karasha <jkarasha@gmail.com>
2025-01-26 21:44:58 -0800

    Update to use Azure OpenAI

--- hello.py
+++ hello.py
@@ -1,13 +1,17 @@
-# !pip install smolagents[litellm]
-from smolagents import CodeAgent, LiteLLMModel
-from dotenv import load_dotenv
 import os
+from smolagents import CodeAgent, AzureOpenAIServerModel
+from dotenv import load_dotenv
+
 
 load_dotenv()
 
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+model = AzureOpenAIServerModel(
+    model_id = "gpt-4o-mini",
+    azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
+    api_key=os.getenv("AZURE_OPENAI_API_KEY"),
+    api_version=os.getenv("OPENAI_API_VERSION")    
+)
 
-model = LiteLLMModel(model_id="openai/gpt-4o", api_key=OPENAI_API_KEY) # Could use 'gpt-4o'
 agent = CodeAgent(tools=[], model=model, add_base_tools=True)
 
 agent.run("Could you give me the 118th number in the Fibonacci sequence?")
